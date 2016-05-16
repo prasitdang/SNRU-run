f2cebb201118b91ea3e041f99ebb7d1d23fbf925
sevenSen <kikkaroclub@gmail.com>
2016-05-16 16:05:55 +0700

    Alert Dialog

--- app/src/main/java/com/dangleetha/prasit/snrurun/SignUp.java
+++ app/src/main/java/com/dangleetha/prasit/snrurun/SignUp.java
@@ -2,13 +2,65 @@ package com.dangleetha.prasit.snrurun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUp extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private EditText nameEditText, userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton, choice5RadioButton;
+    private String nameString, userString, passwordString, avataString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
+
+
+    }// Main Method
+
+    private void bindWidget() {
+
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        choice5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
     }
-}
-/
+
+    public void clickSignUpSign(View view) {
+
+        //Get Value From EditText
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Spece
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") ) {
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องครับ");
+
+        } else {
+        }
+
+
+
+    }
+
+}// Main Class
